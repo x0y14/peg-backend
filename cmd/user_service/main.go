55c5e6eceef6902a8c2c9bd621211d4f32db4e80
x0y14 <ya01ma08@gmail.com>
2022-07-01 17:10:57 +0900

    :white_check_mark: データベースを使用するように変更!

--- cmd/user_service/main.go
+++ cmd/user_service/main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
-	typesv1 "backend/gen/types/v1"
+	"backend/db"
 	userv1 "backend/gen/user/v1"
 	"backend/gen/user/v1/userv1connect"
 	"backend/interceptor"
 	"context"
+	"database/sql"
+	"errors"
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
 	"fmt"
 	"github.com/bufbuild/connect-go"
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 const (
@@ -25,68 +31,97 @@ const (
 type UserServer struct {
 	app  *firebase.App
 	auth *auth.Client
+	db   *sql.DB
 }
 
-func (s *UserServer) Ping(ctx context.Context, req *connect.Request[userv1.PingRequest]) (*connect.Response[userv1.PingResponse], error) {
-	log.Println("Request headers: ", req.Header())
+func (s *UserServer) Ping(_ context.Context, req *connect.Request[userv1.PingRequest]) (*connect.Response[userv1.PingResponse], error) {
 	res := connect.NewResponse(&userv1.PingResponse{Text: fmt.Sprintf("re: %s", req.Msg.Text)})
 	// sample に　あったからつけてみてる。いる?
 	res.Header().Set(ServiceName, ServiceVersion)
 	return res, nil
 }
 
-func (s *UserServer) GetAccount(ctx context.Context, req *connect.Request[userv1.GetAccountRequest]) (*connect.Response[userv1.GetAccountResponse], error) {
-	log.Println("Request headers: ", req.Header())
+func (s *UserServer) GetAccount(_ context.Context, req *connect.Request[userv1.GetAccountRequest]) (*connect.Response[userv1.GetAccountResponse], error) {
+	// 処理要求アカウントのUserIdを取得
+	requesterUserId := req.Header().Get("X-User-Id")
 
-	userId := req.Header().Get("X-User-Id")
-	// Firebase Authを使ってユーザーデータを取り出す
-	userRecode, err := s.auth.GetUser(context.Background(), userId)
+	// DBからアカウント情報取り出し
+	account_, err := db.GetAccount(s.db, requesterUserId)
 	if err != nil {
-		// トークン認証は通ってるので、ものすごい短い間にアカウントを消したか、あるいは。
-		return nil, connect.NewError(connect.CodeInternal, err)
+		switch err {
+		case sql.ErrNoRows:
+			// 一致するものがいない
+			return nil, connect.NewError(connect.CodeNotFound, fmt.Errorf("account not found: %s", requesterUserId))
+		default:
+			// 不明なエラー
+			// todo : log via discord
+			return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("internal error"))
+		}
 	}
-	res := connect.NewResponse(&userv1.GetAccountResponse{Account: &typesv1.Account{
-		UserId:   userRecode.UID,
-		Email:    userRecode.Email,
-		UserName: "", // todo : database
-	}})
+
+	// レスポンス作成
+	res := connect.NewResponse(&userv1.GetAccountResponse{Account: account_})
 	res.Header().Set(ServiceName, ServiceVersion)
 
 	return res, nil
 }
 
-func (s *UserServer) UpdateAccount(ctx context.Context, req *connect.Request[userv1.UpdateAccountRequest]) (*connect.Response[userv1.UpdateAccountResponse], error) {
+func (s *UserServer) UpdateAccount(_ context.Context, _ *connect.Request[userv1.UpdateAccountRequest]) (*connect.Response[userv1.UpdateAccountResponse], error) {
+	// emailを変更可能にするかどうか。
+	// user_nameを別の関数として実装する予定なので、消しても良いかもしれない。
 	return nil, connect.NewError(connect.CodeUnimplemented, fmt.Errorf("UpdateAccount inimplemented"))
 }
 
-func (s *UserServer) GetProfile(ctx context.Context, req *connect.Request[userv1.GetProfileRequest]) (*connect.Response[userv1.GetProfileResponse], error) {
-	log.Println("Request headers: ", req.Header())
+func (s *UserServer) GetProfile(_ context.Context, req *connect.Request[userv1.GetProfileRequest]) (*connect.Response[userv1.GetProfileResponse], error) {
+	// 対象アカウントのUserIdを取得
+	targetUserId := req.Msg.UserId
+	// DBからProfile情報の取得
+	prof, err := db.GetProfile(s.db, targetUserId)
+	if err != nil {
+		switch err {
+		case sql.ErrNoRows:
+			// 一致するものがいない
+			return nil, connect.NewError(connect.CodeNotFound, fmt.Errorf("profile not found: %s", targetUserId))
+		default:
+			return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("internal error"))
+		}
+	}
 
-	userId := req.Header().Get("X-User-Id")
-
-	res := connect.NewResponse(&userv1.GetProfileResponse{Profile: &typesv1.Profile{
-		UserId:        userId,
-		DisplayName:   "", // todo : database
-		IconPath:      "",
-		StatusMessage: "",
-		Metadata:      "",
-	}})
+	res := connect.NewResponse(&userv1.GetProfileResponse{Profile: prof})
 	res.Header().Set(ServiceName, ServiceVersion)
+
 	return res, nil
 }
 
-func (s *UserServer) UpdateProfile(ctx context.Context, req *connect.Request[userv1.UpdateProfileRequest]) (*connect.Response[userv1.UpdateProfileResponse], error) {
-	log.Println("Request headers: ", req.Header())
+func (s *UserServer) UpdateProfile(_ context.Context, req *connect.Request[userv1.UpdateProfileRequest]) (*connect.Response[userv1.UpdateProfileResponse], error) {
+	requesterUserId := req.Header().Get("X-User-Id")
 
-	//userId := req.Header().Get("X-User-Id")
+	// optional, nili以外をアップデートします。
+	newProfile, err := db.UpdateProfile(s.db, requesterUserId, req.Msg)
+	if err != nil {
+		log.Printf("%v", err)
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, connect.NewError(connect.CodeNotFound, fmt.Errorf("profile not found: %s", requesterUserId))
+		case errors.Is(err, err.(*mysql.MySQLError)):
+			mysqlErr := err.(*mysql.MySQLError)
+			switch mysqlErr.Number {
+			case 4025:
+				return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("database error: CONSTRAINT"))
+			default:
+				return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("database error: %v", mysqlErr.Number))
+			}
+		case errors.Is(err, err.(*connect.Error)):
+			return nil, err
+		default:
+			return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("internal error"))
+		}
+	}
 
-	//optional string display_name = 2;
-	//optional string icon_path = 3;
-	//optional string status_message = 4;
-	//optional string metadata = 5;
+	res := connect.NewResponse(&userv1.UpdateProfileResponse{Profile: newProfile})
+	res.Header().Set(ServiceName, ServiceVersion)
 
-	//s.auth.UpdateUser(context.Background(), userId)
-	return nil, connect.NewError(connect.CodeUnimplemented, fmt.Errorf("UpdateProfile inimplemented"))
+	return res, nil
 }
 
 func main() {
@@ -106,9 +141,22 @@ func main() {
 		log.Fatalf("error getting Auth client: %v\n", err)
 	}
 
+	database, err := sql.Open("mysql", fmt.Sprintf("%s:%s@/%s",
+		"root",
+		os.Getenv("MARIADB_ROOT_PASSWORD"),
+		os.Getenv("MARIADB_DATABASE")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
+	database.SetConnMaxLifetime(time.Minute * 3)
+	database.SetMaxOpenConns(10)
+	database.SetMaxIdleConns(10)
+
 	userServer := &UserServer{
 		app:  app,
 		auth: client,
+		db:   database,
 	}
 	mux := http.NewServeMux()
 	interceptors := connect.WithInterceptors(
@@ -117,8 +165,10 @@ func main() {
 		userServer,
 		interceptors,
 	))
-	http.ListenAndServe(
+	if err = http.ListenAndServe(
 		"localhost:8080",
 		h2c.NewHandler(mux, &http2.Server{}),
-	)
+	); err != nil {
+		log.Fatal(err)
+	}
 }
